c6da6aa968037bea455913647afd34ad662e52f4
Bhavithayelamati <109526473+Bhavithayelamati@users.noreply.github.com>
2023-12-14 21:31:01 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,12 @@
-favorite_fruits = ["apple", "banana", "orange", "strawberry", "kiwi"]
+favorite_fruits = [ "banana",  "strawberry", "apple" ,"kiwi","orange",]
 word_list = favorite_fruits
 print(word_list)
+import random
+word = random.choice(word_list)
+print(word)
+guess = input("Enter a single letter: ")
+# Check if the input is a single character and alphabetical
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
