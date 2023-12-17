83a67c24758c5f87b7def8744e8dc29690933c92
Bhavithayelamati <109526473+Bhavithayelamati@users.noreply.github.com>
2023-12-17 12:54:36 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,19 +1,36 @@
 import random
 
-def validate_input(guess):
-    # Check if the input is a single character and alphabetical
+def choose_random_word(word_list):
+    return random.choice(word_list)
+
+def get_user_guess():
+    return input("Enter a single letter: ")
+
+def validate_guess(guess):
     if len(guess) == 1 and guess.isalpha():
         print("Good guess!")
     else:
         print("Oops! That is not a valid input.")
 
+# Task 1: Define the list of possible words
 favorite_fruits = ["banana", "strawberry", "apple", "kiwi", "orange"]
+
+# Assign this list to a variable called word_list
 word_list = favorite_fruits
 
+# Print out the newly created list to the standard output (screen)
 print(word_list)
 
-word = random.choice(word_list)
+# Task 2: Choose a random word from the list
+word = choose_random_word(word_list)
+
+# Assign the randomly generated word to a variable called word
 print(word)
 
-guess = input("Enter a single letter: ")
-validate_input(guess)
+# Task 3: Ask the user for an input
+# Using the input function, ask the user to enter a single letter
+# Assign the input to a variable called guess
+guess = get_user_guess()
+
+# Task 4: Check that the input is a single character
+validate_guess(guess)
