8a1290a0f2ca57ad59fd2e5f00998d8f638d90b6
Bhavithayelamati <109526473+Bhavithayelamati@users.noreply.github.com>
2023-12-17 11:30:52 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,12 +1,27 @@
 import random
 
-def validate_input(guess):
-    # Check if the input is a single character and alphabetical
-    if len(guess) == 1 and guess.isalpha():
-        return True
+def check_guess(word, guess):
+    # Step 2: Convert the guess into lower case
+    guess = guess.lower()
+
+    # Step 3: Check if the guess is in the word
+    if guess in word:
+        print(f"Good guess! '{guess}' is in the word.")
     else:
-        print("Oops! That is not a valid input.")
-        return False
+        print(f"Sorry, '{guess}' is not in the word. Try again.")
+
+def ask_for_input():
+    while True:
+        # Step 2: Move the code for input validation into ask_for_input
+        guess = input("Enter a single letter: ")
+
+        # Validate the user's input
+        if len(guess) == 1 and guess.isalpha():
+            # Step 3: Call check_guess to check if the guess is in the word
+            check_guess(word, guess)
+            break
+        else:
+            print("Oops! That is not a valid input.")
 
 # List of favorite fruits
 favorite_fruits = ["banana", "strawberry", "apple", "kiwi", "orange"]
@@ -18,25 +33,13 @@ print("List of words:", word_list)
 while True:
     # Randomly choose a word
     word = random.choice(word_list)
-    
+
     # Display a placeholder for the word (you may replace this with your actual game logic)
     hidden_word = ['_' for _ in word]
     print(" ".join(hidden_word))
 
-    # Ask the user to guess a letter
-    guess = input("Enter a single letter: ")
-
-    # Validate the user's input
-    if validate_input(guess):
-        # Step 1: Check if the guess is in the word
-        if guess in word:
-            # Step 2: Print a message for a correct guess
-            print(f"Good guess! '{guess}' is in the word.")
-        else:
-            # Step 3: Print a message for an incorrect guess
-            print(f"Sorry, '{guess}' is not in the word. Try again.")
-
-    # Optionally, you can add logic here to update the displayed word based on the correct guesses
+    # Step 4: Call ask_for_input outside the while loop to test the code
+    ask_for_input()
 
     # Check if the user wants to play again
     play_again = input("Do you want to play again? (yes/no): ").lower()
