8ad9417fce4f1e18bc64fb6acf1c771fba1183a5
Bhavithayelamati <109526473+Bhavithayelamati@users.noreply.github.com>
2023-12-17 12:26:32 +0000

    Update milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -1,8 +1,19 @@
-# Ask the user to enter a single letter
-guess = input("Enter a single letter: ")
+import random
 
-# Check if the input is a single character and alphabetical
-if len(guess) == 1 and guess.isalpha():
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input.")
+def validate_input(guess):
+    # Check if the input is a single character and alphabetical
+    if len(guess) == 1 and guess.isalpha():
+        print("Good guess!")
+    else:
+        print("Oops! That is not a valid input.")
+
+favorite_fruits = ["banana", "strawberry", "apple", "kiwi", "orange"]
+word_list = favorite_fruits
+
+print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
+guess = input("Enter a single letter: ")
+validate_input(guess)
