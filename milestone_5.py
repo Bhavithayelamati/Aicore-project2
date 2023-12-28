80801bc8ab8442a681436e8a2d376465159981c1
Bhavithayelamati <109526473+Bhavithayelamati@users.noreply.github.com>
2023-12-28 19:26:26 +0000

    Update milestone_5.py

--- milestone_5.py
+++ milestone_5.py
@@ -2,8 +2,16 @@
 
 import random
 
+
 class Hangman:
     def __init__(self, word_list, num_lives=5):
+        """
+        Initialize the Hangman game.
+
+        Parameters:
+        - word_list: List of words to choose from.
+        - num_lives: Number of lives the player starts with.
+        """
         self.word_list = word_list
         self.num_lives = num_lives
         self.word = self.choose_random_word()
@@ -12,17 +20,29 @@ class Hangman:
         self.list_of_guesses = []
 
     def choose_random_word(self):
+        """
+        Choose a random word from the word list.
+
+        Returns:
+        - Randomly chosen word.
+        """
         return random.choice(self.word_list)
 
     def check_guess(self, guess):
+        """
+        Check if the guessed letter is in the word.
+
+        Parameters:
+        - guess: The letter guessed by the player.
+        """
         guess = guess.lower()
 
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
 
-            for i, letter in enumerate(self.word):
+            for index, letter in enumerate(self.word):
                 if letter == guess:
-                    self.word_guessed[i] = guess
+                    self.word_guessed[index] = guess
                     self.num_letters -= 1
         else:
             self.num_lives -= 1
@@ -30,6 +50,9 @@ class Hangman:
             print(f"You have {self.num_lives} lives left.")
 
     def ask_for_input(self):
+        """
+        Ask the player to guess a letter and validate the input.
+        """
         while True:
             guess = input("Guess a letter: ")
 
@@ -37,12 +60,16 @@ class Hangman:
                 if guess not in self.list_of_guesses:
                     self.list_of_guesses.append(guess)
                     self.check_guess(guess)
+                    break
                 else:
                     print("You already tried that letter!")
             else:
                 print("Invalid letter. Please, enter a single alphabetical character.")
 
     def play_game(self):
+        """
+        Play the Hangman game until the player wins or loses.
+        """
         while True:
             if self.num_lives == 0:
                 print("You lost!")
@@ -53,9 +80,19 @@ class Hangman:
                 print("Congratulations. You won the game!")
                 break
 
+
 def play_game(word_list):
+    """
+    Start the Hangman game with the given word list.
+
+    Parameters:
+    - word_list: List of words for the game.
+    """
     num_lives = 5
     game = Hangman(word_list, num_lives)
     game.play_game()
 
 
+if __name__ == "__main__":
+    word_list = ['apple', 'banana']
+    play_game(word_list)
